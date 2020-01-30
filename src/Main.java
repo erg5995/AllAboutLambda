3eb98a554fc3ff64db90c8bb3d5cbb9271475999
erg5995 <54483064+erg5995@users.noreply.github.com>
2020-01-30 15:39:36 -0500

    Add files via upload
    Initial Commit of first basic demonstration.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Test basic = new BasicTest();
+        Test extended = new ExtendedTest();
+
+        System.out.println("----TEST BASIC----");
+        basic.run();
+        System.out.println("----TEST EXTENDED----");
+        extended.run();
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public abstract class Test {
+
+    /** State for this example: a list of integers and random number generator. */
+    protected static Random random = new Random();
+    protected static List<Integer> list = new ArrayList<>();
+
+    /** The function which will be receiving our lambda functions. */
+    protected abstract List<Integer> test(Filter filter);
+    /** Where the test will actually be run */
+    protected abstract void run();
+
+    /**
+     * Initializes the integer array with random integers 0-9.
+     * @param size The number of random integers to generate.
+     */
+    protected void refreshIntegers(int size){
+        list = new ArrayList<>();
+        for(int i = 0; i < size; i++){
+            list.add(random.nextInt(10));
+        }
+    }
+
+}
